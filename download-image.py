86fb47357bbb232634a000290b263b00880f0792
Debjyoti Chowdhury <djbabai.debjyoti@gmail.com>
2024-03-18 20:08:37 +0530

    mkdir ability

--- download-image.py
+++ download-image.py
@@ -1,5 +1,5 @@
-from os import listdir
-from os.path import join, basename
+from os import listdir, makedirs
+from os.path import join, basename, exists
 import requests
 import pandas as pd
 
@@ -8,12 +8,21 @@ download_dir = "dataset/george_test_task"
 
 all_targets = listdir(image_link_path)
 
+# Check if download directory exists, if not create it
+if not exists(download_dir):
+    makedirs(download_dir)
+
 for csv_file in all_targets:
     full_path = join(image_link_path, csv_file)
     print(f"Processing image links from {csv_file}")
     df = pd.read_csv(full_path)
 
     image_folder_name = csv_file.split(".")[0]
+
+    image_folder_path = join(download_dir, image_folder_name)
+    # Check if image folder path exists, if not create it
+    if not exists(image_folder_path):
+        makedirs(image_folder_path)
 
     image_urls = df.iloc[:, 0]
 
@@ -25,3 +34,4 @@ for csv_file in all_targets:
             filename = basename(image_url)
             with open(join(download_dir, image_folder_name, filename), 'wb') as img_file:
                 img_file.write(response.content)
+
